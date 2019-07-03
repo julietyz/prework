3e30e9cd23d229ee788d8840d10aea5460731ca8
Juliet Yznaga <juliet.yznaga@duke.edu>
2019-07-03 13:09:26 +0200

    Some changes

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,8 @@
-window.toggleLeft = function() {
-    var element = document.getElementById("offcanvas-left");
-    element.classList.toggle("hide");
-}
-window.toggleRight = function() {
-    var element = document.getElementById("offcanvas-right");
-    element.classList.toggle("hide");
-}
+ window.toggleLeft = function() {
+     var element = document.getElementById("offcanvas-left");
+     element.classList.toggle("hide");
+ }
+ window.toggleRight = function() {
+     var element = document.getElementById("offcanvas-right");
+     element.classList.toggle("hide");
+ }
